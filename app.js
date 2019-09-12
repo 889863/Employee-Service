10f8a2fa9d952681f21d933d281a0beeaac42748
Subin John <subinjohn.in@gmail.com>
2019-09-12 01:13:59 -0500

    Adding Delete

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use((req, res, next)=>{
     res.header("Access-Control-Allow-headers", "*");
     
     if(req.method === 'OPTIONS'){
-        res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELTE");
+        res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
         return res.status(200).json({});
     }
     next();
